dbd1ab4fd23d97ab24303c2090777353daf12dbb
Walid Siala <wsiala4@gmail.com>
2025-09-17 14:38:00 +0200

    Added Paper type

---
+++ src/types/paper.ts
@@ -0,0 +1,14 @@
+export type Paper = {
+    paper_id: string;
+    title: string;
+    authors: string[];
+    abstract: string;
+    url: string;
+    published_date: Date;
+    updated_date: Date;
+    source: string;
+    categories: string[];
+    keywords: string[];
+    doi: string;
+    pdf_url: string;
+}
